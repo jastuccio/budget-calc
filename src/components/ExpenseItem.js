3fc20fba2be3a2549f2b2533df3767649bc70b24
Joe <joe@astucc.io>
2019-09-30 10:15:23 -0400

    add components and uuid for list items

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <div>
+item
+    </div>
+  )
+}
+
+export default ExpenseItem
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Item from './ExpenseItem'
+
+const ExpenseList = () => {
+  return (
+    <div>
+expense list
+    <Item />
+    </div>
+
+  )
+}
+
+export default ExpenseList
