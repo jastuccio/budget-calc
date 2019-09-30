feb3913d6ddab6d127daea85948f6086ea138e10
Joe <joe@astucc.io>
2019-09-30 15:57:20 -0400

    feat: update expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import {mdSend} from 'react-icons/md'
 
 const ExpenseForm = () => {
   return (
-    <div>
-form
-    </div>
-  )
+    <form>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g. rent"
+          />
+          </div>
+          <div className="form-group">
+            <label htmlFor="amount">amount</label>
+            <input
+              type="text"
+              className="form-control"
+              id="amount"
+              name="amount"
+              placeholder="e.g. 100"
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn">
+          submit
+          <mdSend className="btn-icon"/>
+        </button>
+    </form>
+)
 }
 
 export default ExpenseForm
