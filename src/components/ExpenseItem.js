9ab8e4a9e4706c024db394d71eeb1af496e44257
Joe <joe@astucc.io>
2019-09-30 17:08:17 -0400

    feat: update components list and item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,10 @@
 import React from 'react'
-import {MdEdit, MdDelete} from 'react-icons/md'
 
-const ExpenseItem = ({ expense }) => {
-  const { id, charge, amount } = expense
-
+const ExpenseItem = () => {
   return (
-    <li className="item">
-      <div className="info">
-        <span className="expense">{charge}</span>
-        <span className="amount">${amount}</span>
-      </div>
-      <div>
-        <button className="edit-btn" aria-label="edit button">
-          <MdEdit />
-        </button>
-        <button className="delete-btn" aria-label="delete button">
-          <MdDelete />
-        </button>
-      </div>
-    </li>
+    <div>
+item
+    </div>
   )
 }
 
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,22 +1,12 @@
 import React from 'react'
 import Item from './ExpenseItem'
-import {MdDelete} from 'react-icons/md'
 
-const ExpenseList = ({expenses}) => {
+const ExpenseList = () => {
   return (
-    <>
-      <ul className="list">
-        {expenses.map((expense)=>{
-          return <Item key={expense.id} expense={expense} />
-        })}
-      </ul>
-      {expenses.length > 0 &&(
-        <button className="btn">
-        clear expenses
-        <MdDelete className="btn-icon"/>
-        </button>
-      )}
-    </>
+    <div>
+expense list
+    <Item />
+    </div>
 
   )
 }
